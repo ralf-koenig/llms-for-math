554c453936cf37dcf66f667533fdd4141883b4d9
fritz <koe.f@posteo.com>
2025-11-27 15:37:48 +0100

    Refactor ask_blablador.py, Add repl.py
    repl contains only functions for parsing and compiling lean4 script on the command line

---
+++ src/lean4/prompting/repl.py
@@ -0,0 +1,31 @@
+
+import argparse
+import subprocess
+
+def parse_lean_to_repl(lean):
+    replaced_newlines = lean.replace("\n", "\\n")
+    cmd_string = f'{{ "cmd": "{replaced_newlines}" }}'
+    return cmd_string
+
+def execute_lean(repl_json):
+    # maybe add a optional to specify where the repl compiler is located, if not in current directory
+    repl_cmd = subprocess.run(['lake', 'exe', 'repl'], input=repl_json, stdout=subprocess.PIPE, encoding='utf-8')
+    return repl_cmd.stdout
+
+def compile_lean(lean_script):
+    command_line_string = parse_lean_to_repl(lean_script)
+    return execute_lean(command_line_string)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='execute_lean',
+        description='takes a lean script as single String and executes it by using lean4 repl')
+    parser.add_argument('lean_script', type=str, help='the lean script to execute, must be a String')
+    args = parser.parse_args()
+    # the only validate input should be a lean script, so maybe validate the input
+    print(compile_lean(args.lean_script))
+
+    #with open("blablador_response.json", "w") as f:
+    #    f.write(json.dumps(output))
+    #output_json = output
+    # print("Output: ", output_json["choices"][0]["text"])
