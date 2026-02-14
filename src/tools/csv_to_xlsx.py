6b59733544ff8274d1613f4868435a1d9e3fbc48
Ralf Koenig <104648179+ralf-koenig@users.noreply.github.com>
2026-02-14 21:11:31 +0100

    tool for conversion of streamlit results to Excel for better analysis in Excel

--- src/tools/csv_to_xlsx.py
+++ src/tools/csv_to_xlsx.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pathlib import Path
 
-csv_file = Path("../python/data/autosaves/results_2026-02-11_14-02-10.csv")
+csv_file = Path("../python/data/autosaves/results_2026-02-09_19-35-36.csv")
 xlsx_file = csv_file.with_suffix(".xlsx")
 
 df = pd.read_csv(csv_file)
